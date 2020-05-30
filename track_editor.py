4c8a120469789d29cfda35dd137e752498930ae1
alguerre <alguerre@outlook.com>
2020-05-31 00:45:37 +0200

    Create basic tkinter GUI with the plot

--- track_editor.py
+++ track_editor.py
@@ -1,65 +1,32 @@
-import gpx
 import logging
 import datetime as dt
 import os
+import tkinter
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import matplotlib.gridspec as gridspec
+import matplotlib.backends.backend_tkagg as  backend_tkagg  #import (FigureCanvasTkAgg, NavigationToolbar2Tk)
+import matplotlib.backend_bases as backend_bases  #key_press_handler
+import matplotlib.figure as figure  #import Figure
 import numpy as np
+import pandas as pd
 
+
+import gpx
 import constants as c
 import iosm
+import utils
 
 
-def auto_zoom(lat_min: float, lon_min: float,
-              lat_max: float, lon_max: float) -> int:
-    """
-    Compute the best zoom to show a complete track. It must contain the full
-    track in more than one tail but minimising the number of tails.
-    :param lat_min: furthest south point
-    :param lon_min: furthest west point
-    :param lat_max: furthest north point
-    :param lon_max: furthest east point
-    :return: zoom to use to show full track
-    """
-
-    for zoom in range(c.max_zoom):
-        num_x_min, num_y_min = iosm.deg2num(lat_min, lon_min, zoom)
-        num_x_max, num_y_max = iosm.deg2num(lat_max, lon_max, zoom)
-
-        number_tiles = abs((num_x_max - num_x_min) * (num_y_max - num_y_min))
-
-        if number_tiles > 1:
-            return zoom
-
-    return c.max_zoom
-
-
-if __name__ == "__main__":
-    # Define logger
-    if not os.path.isdir("log"):
-        os.mkdir("log")
-
-    now = dt.datetime.now()  # current date and time
-    date_time = now.strftime("%Y%m%d-%H%M%S")
-
-    logging.basicConfig(level=c.log_level,
-                        filename=f"log/{date_time}_track_editor.log")
-    logger = logging.getLogger()
-
-    # Load gpx file
-    my_route = "test_cases/nominal_route.gpx"
-    gpx_track = gpx.Gpx(my_route)
-    pd_track = gpx_track.to_pandas()
-
+def plot_gpx(df_track: pd.DataFrame):
     # Get extremes values
-    lat_min = pd_track["lat"].min()
-    lat_max = pd_track["lat"].max()
-    lon_min = pd_track["lon"].min()
-    lon_max = pd_track["lon"].max()
+    lat_min = df_track["lat"].min()
+    lat_max = df_track["lat"].max()
+    lon_min = df_track["lon"].min()
+    lon_max = df_track["lon"].max()
 
     # Select optimal zoom
-    zoom = auto_zoom(lat_min, lon_min, lat_max, lon_max)
+    zoom = utils.auto_zoom(lat_min, lon_min, lat_max, lon_max)
 
     # Download OSM tiles if needed
     iosm.download_tiles(lat_min, lon_min, lat_max, lon_max)
@@ -93,11 +60,11 @@ if __name__ == "__main__":
     ax = plt.gca()
     ymax, xmax = iosm.num2deg(final_xtile+1, ytile, zoom)
     ymin, xmin = iosm.num2deg(xtile, final_ytile+1, zoom)
-    BBox = (xmin, xmax, ymin, ymax)
-    ax.imshow(map_img, zorder=0, extent=BBox, aspect='equal')
+    bbox = (xmin, xmax, ymin, ymax)
+    ax.imshow(map_img, zorder=0, extent=bbox, aspect='equal')
 
     # Plot track
-    ax.scatter(pd_track.lon, pd_track.lat, s=5)
+    ax.scatter(df_track.lon, df_track.lat, s=5)
 
     # Beauty salon
     plt.tick_params(axis="x", bottom=False, top=False, labelbottom=False)
@@ -110,11 +77,46 @@ if __name__ == "__main__":
     with plt.style.context('ggplot'):
         plt.subplot(gspec[3, 0])
         ax = plt.gca()
-        ax.fill_between(np.arange(len(pd_track)), pd_track.ele)
-        ax.set_ylim((pd_track.ele.min()*0.8, pd_track.ele.max()*1.2))
+        ax.fill_between(np.arange(len(df_track)), df_track.ele)
+        ax.set_ylim((df_track.ele.min()*0.8, df_track.ele.max()*1.2))
 
-    # TODO: tkinter https://www.youtube.com/watch?v=jBUpjijYtCk
-    # TODO: tutoriales tkinter: https://www.youtube.com/playlist?list=PLQVvvaa0QuDclKx-QpC9wntnURXVJqLyk
+    return plt.gcf()  #plt.show()
 
 
-    plt.show()
+def _quit():
+    root.quit()     # stops mainloop
+    root.destroy()  # this is necessary on Windows to prevent
+                    # Fatal Python Error: PyEval_RestoreThread: NULL tstate
+
+
+if __name__ == "__main__":
+    # Define logger
+    if not os.path.isdir("log"):
+        os.mkdir("log")
+
+    now = dt.datetime.now()  # current date and time
+    date_time = now.strftime("%Y%m%d-%H%M%S")
+
+    logging.basicConfig(level=c.log_level,
+                        filename=f"log/{date_time}_track_editor.log")
+    logger = logging.getLogger()
+
+    # Load gpx file
+    my_route = "test_cases/nominal_route.gpx"
+    gpx_track = gpx.Gpx(my_route)
+    df_gpx = gpx_track.to_pandas()
+    
+    # Plot 
+    fig = plot_gpx(df_gpx)
+
+    root = tkinter.Tk()
+    root.wm_title("Embedding in Tk")
+
+    canvas = backend_tkagg.FigureCanvasTkAgg(fig, master=root)
+    canvas.draw()
+    canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
+
+    button = tkinter.Button(master=root, text="Quit", command=_quit)
+    button.pack(side=tkinter.BOTTOM)
+
+    tkinter.mainloop()
